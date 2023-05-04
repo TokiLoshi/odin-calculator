b8ca1834c7bfc15a28a3c4023e14ed4821cabdab
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2023-05-03 17:16:02 -0700

    Fixed issue with second number not joining

--- app.js
+++ app.js
@@ -67,15 +67,165 @@ function parseInput(userInput){
       console.log(`We have an operand ${problem[i]} at iteration ${i}`)
       firstNum = parseInt(problem.slice(0, i).join(''))
       operand = problem[i]
-      secondNum = parseInt(problem.slice(i+1))
+      secondNum = parseInt(problem.slice(i+1).join(''))
       console.log(`First num: ${firstNum}`)
       console.log(`Operand: ${operand}`)
       console.log(`Second num: ${secondNum}`)
-      let remainingArray = problem.slice(i+1)
+      let remainingArray = problem.slice(i+1).join('')
       console.log(`RemainingArray ${remainingArray}`)
 
       
-      // Everything here is code to allow it to pass the full array
+      // Answer should be updated with operate on first and second number
+      answer = operate(firstNum, operand, secondNum)
+      console.log(`Answer is: ${answer}`)
+      return answer;
+    }
+  }
+  // return answer;
+}
+
+// Store everything in array 
+function getInput(){
+  // Add eventlisteners to all buttons
+  let userInput = []
+
+  // Update screen as user adds input
+  const screen = document.querySelector('.screen')
+  let firstOperator = ""
+  let secondOperator = ""
+
+  // Get the number buttons 
+  const buttons = document.querySelectorAll('.numbutton')
+  buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const numclick = button.id
+
+    // Handle if clear button was used - should get rid of the zero
+    // This causes bugs later
+    if (userInput[0] === 0){
+
+      userInput = []
+    }
+    userInput.push(numclick)
+    screen.textContent = userInput.join(' ')
+    
+  })
+})
+
+const operations = document.querySelectorAll('.opbutton')
+operations.forEach((button) => {
+  button.addEventListener('click', () => {
+    console.log(`Length of user Input: ${userInput.length}`)
+    const opclick = button.id
+
+    // One option might be to create two operators and store them
+    if (opclick === 'plus'){
+      userInput.push('+')
+      if (firstOperator === ""){
+        firstOperator = '+'
+      }
+      else{
+        secondOperator = '+' 
+      }
+    }
+    else if (opclick === 'minus'){
+      userInput.push('-')
+      if (firstOperator === ""){
+        firstOperator = '-'
+      }
+      else{
+        secondOperator = '-' 
+      }
+    }
+    else if (opclick === 'multiply'){
+      userInput.push('x')
+      if (firstOperator === ""){
+        firstOperator = 'x'
+      }
+      else{
+        secondOperator = 'x' 
+      }
+    }
+    else if (opclick === 'divide'){
+      userInput.push('/')
+      if (firstOperator === ""){
+        firstOperator = '/'
+      }
+      else{
+        secondOperator = '/' 
+      }
+    }
+    // console.log(`Checked for + - x and / and we have userInput: ${userInput}`)
+    // console.log(`First operator: ${firstOperator}`)
+    // console.log(`Second Operator: ${secondOperator}`)
+    // console.log(`Length of userinput after assigning operator: ${userInput.length}`)
+    
+    if (firstOperator !== '' && secondOperator !== ''){
+      let toCompute = userInput.slice(0, (userInput.length -1))
+      console.log(`To compute: ${toCompute}`)
+      answer = parseInput(toCompute)
+      console.log(`Getting an answer: ${answer}`)
+      userInput = [answer]
+      userInput.push(secondOperator)
+    }
+
+    // I know this is going to cause bugs later
+    if (opclick === 'equal' && firstOperator !== ''){
+      console.log('User hit equals')
+      answer = parseInput(userInput)
+      userInput = [answer]
+    }
+    if (opclick === 'clear'){
+      userInput = [0]
+    }
+
+    // else if (opclick === 'equal'){
+    //   userInput.join(' ')
+    //   let answer = parseInput(userInput)
+    //   console.log(`We got the answer back in operations function: ${answer}`)
+    //   userInput = [answer]
+    //   screen.textContent = userInput
+    // }
+    // else if (opclick === 'plus'){
+    //   operatorbutton = '+'
+    //   userInput.push(operatorbutton)
+    // }
+    // else if(opclick === 'minus'){
+    //   userInput.push('-')
+    // }
+    // else if(opclick === 'divide'){
+    //   userInput.push('/')
+    // }
+    // else if(opclick === 'multiply'){
+    //   userInput.push('x')
+    // }
+    // else if(opclick === 'clear'){
+    //   userInput = [0]
+    // }
+    // else {
+    //   userInput.push(opclick)
+      
+    // }
+    //Update screen
+    screen.textContent = userInput.join(' ')
+  })
+})
+ 
+}
+
+let input = getInput()
+if (input){
+  console.log(`What's being returned: ${input}`)
+}
+
+
+
+
+
+
+
+      // // From ParseInput Function: 
+      // This allowed users to input multiple operations 
       // if(i === lengthInput -1){
       //   return answer}
       //   else{
@@ -121,136 +271,3 @@ function parseInput(userInput){
       //   console.log(`Second number with no remaining array: ${secondNum}`)
       // }
       // console.log(`Time to calculate the answer with ${firstNum} a ${typeof firstNum} operator ${operand} and ${secondNum} a ${typeof secondNum}`)
-      
-      // Answer should be updated with operate on first and second number
-      answer = operate(firstNum, operand, secondNum)
-      console.log(`Answer is: ${answer}`)
-    }
-  }
-  return answer;
-}
-
-// Store everything in array 
-function getInput(){
-  // Add eventlisteners to all buttons
-  let userInput = []
-
-  // Update screen as user adds input
-  const screen = document.querySelector('.screen')
-  let firstOperator = ""
-  let secondOperator = ""
-
-  // Get the number buttons 
-  const buttons = document.querySelectorAll('.numbutton')
-  buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const numclick = button.id
-
-    // Handle if clear button was used - should get rid of the zero
-    if (userInput[0] === 0){
-      userInput = []
-    }
-    userInput.push(numclick)
-    screen.textContent = userInput.join(' ')
-    
-  })
-})
-
-const operations = document.querySelectorAll('.opbutton')
-operations.forEach((button) => {
-  button.addEventListener('click', () => {
-    console.log(`Length of user Input: ${userInput.length}`)
-    const opclick = button.id
-    // console.log(`Button clicked: ${opclick}`)
-    // console.log(`UserInput so far: ${userInput}`)
-
-    // One option might be to create two operators and store them
-    // If User Input greater than two we evaluate 
-
-    if (opclick === 'plus'){
-      userInput.push('+')
-      if (firstOperator === ""){
-        firstOperator = '+'
-      }
-      else{
-        secondOperator = '+' 
-      }
-    }
-    else if (opclick === 'minus'){
-      userInput.push('-')
-      if (firstOperator === ""){
-        firstOperator = '-'
-      }
-      else{
-        secondOperator = '-' 
-      }
-    }
-    else if (opclick === 'multiply'){
-      userInput.push('x')
-      if (firstOperator === ""){
-        firstOperator = 'x'
-      }
-      else{
-        secondOperator = 'x' 
-      }
-    }
-    else if (opclick === 'divide'){
-      userInput.push('/')
-      if (firstOperator === ""){
-        firstOperator = '/'
-      }
-      else{
-        secondOperator = '/' 
-      }
-    }
-    // console.log(`Checked for + - x and / and we have userInput: ${userInput}`)
-    // console.log(`First operator: ${firstOperator}`)
-    // console.log(`Second Operator: ${secondOperator}`)
-    // console.log(`Length of userinput after assigning operator: ${userInput.length}`)
-    if (firstOperator !== '' && secondOperator !== ''){
-      let toCompute = userInput.slice(0, (userInput.length -1))
-      console.log(`To compute: ${toCompute}`)
-      answer = parseInput(toCompute)
-      console.log(`Getting an answer: ${answer}`)
-      userInput = [answer]
-      userInput.push(secondOperator)
-    }
-
-    // else if (opclick === 'equal'){
-    //   userInput.join(' ')
-    //   let answer = parseInput(userInput)
-    //   console.log(`We got the answer back in operations function: ${answer}`)
-    //   userInput = [answer]
-    //   screen.textContent = userInput
-    // }
-    // else if (opclick === 'plus'){
-    //   operatorbutton = '+'
-    //   userInput.push(operatorbutton)
-    // }
-    // else if(opclick === 'minus'){
-    //   userInput.push('-')
-    // }
-    // else if(opclick === 'divide'){
-    //   userInput.push('/')
-    // }
-    // else if(opclick === 'multiply'){
-    //   userInput.push('x')
-    // }
-    // else if(opclick === 'clear'){
-    //   userInput = [0]
-    // }
-    // else {
-    //   userInput.push(opclick)
-      
-    // }
-    //Update screen
-    screen.textContent = userInput.join(' ')
-  })
-})
- 
-}
-
-let input = getInput()
-if (input){
-  console.log(`What's being returned: ${input}`)
-}
